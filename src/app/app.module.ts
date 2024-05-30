529bcb1dad013efad8e4d9e882ba1a92b8d65cf2
Mauro <maurodelle7@gmail.com>
2024-05-30 17:52:29 -0300

    Subiendo Cascara

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase:{
+        apiKey: "AIzaSyDZ5nfzIElrFuyUPzGPdn0DbuW5c4fTaZw",
+        authDomain: "primer-parcial-a24b0.firebaseapp.com",
+        projectId: "primer-parcial-a24b0",
+        storageBucket: "primer-parcial-a24b0.appspot.com",
+        messagingSenderId: "1064710219109",
+        appId: "1:1064710219109:web:cbbb4add0547ffaa89ad8c"
+    }
+};
